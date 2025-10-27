fa68d233dab5ae054cae94d5e048323a381e64ec
LeonardoRomulo <leo.romulo15@gmail.com>
2025-10-27 11:00:51 -0300

    feat(controller): Implementando o hash de senha ao criar usuario e o metodo de listar usuario

--- Controller/usuarioController.js
+++ Controller/usuarioController.js
@@ -1,30 +1,36 @@
 import conexao from "../Model/conexao.js";
 import Usuario from "../Model/usuario.js";
+import bcrypt from "bcrypt";
 
 class UsuarioController {
-    static async criarUsuario(res, req) {
+    static async criarUsuario(req, res) {
         try {
             const { nome, email, senha } = req.body;
-            const usuario = new Usuario(nome, email, senha);
+            const senhaHash = await bcrypt.hash(senha, 10);
+            const usuario = new Usuario(nome, email, senhaHash);
 
-            const query = 'INSERT INTO (nome, email, senha) VALUES (?, ?, ?)';
-            await pool.query(query, [usuario.nome, usuario.email, usuario.senha]);
-            return res.status(200).json({ message: "Usuário cadastro com sucesso" });
+            const query = 'INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)';
+            await conexao.query(query, [usuario.nome, usuario.email, usuario.senha]);
+
+            return res.status(201).json({id:result.insertId, message: "Usuário cadastrado com sucesso" });
         } catch (err) {
-            return res.status(500).json({ err: err.message });
+            return res.status(400).json({message: err.message});
         }
     }
 
     static async listarUsuario(req, res) {
         try {
-
+            const query = 'SELECT * FROM usuarios';
+            const [usuarios] = await conexao.query(query);
+            return res.status(200).json(usuarios);
         } catch (err) {
-
+            return res.status(500).json({ message: "Erro ao listar o usuário" });
         }
     }
 
-    static async atulizarUsuario(req,res) {
+    static async atualizarUsuario(req, res) {
         try {
+            const{id} = req.params.id;
 
         } catch (err) {
 
